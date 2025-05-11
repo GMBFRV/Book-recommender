ef22f1af59a89413dd305d999ba1b062b443a89b
Renat <98940020+GMBFRV@users.noreply.github.com>
2025-05-11 15:11:38 +0700

    #9 Deleted config.py

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes"
-API_KEY = 'AIzaSyCuKsGvqGPO_WTv8doiBp4yF5k9hqHCRuc'
-if not GOOGLE_BOOKS_URL or not API_KEY:
-    raise RuntimeError("Не задані GOOGLE_BOOKS_URL або API_KEY")
